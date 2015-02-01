32dbbf4f3e899cd16ea62afd9c55e4f5eef3c96b
vav17 <mail@vav17.com>
2015-02-01 14:27:04 +0100

    /NSkill fixed

--- NoobScript.js
+++ NoobScript.js
@@ -8,15 +8,15 @@ function startScript() {
 }
 setTimeout(startScript, 200);
 
-function noobScript() {
+var noobScript = function() {
     //onStartup
     $('<link type="text/css" rel="Styles" href="https://rawgit.com/vav17/NoobScript-331/master/Styles.css"/>').appendTo("#app")
     $('<div id="Intro" style ="color: #4169E1; border: 1px solid #D3D3D3; font-size: 14pt; font-family: Lucida Console; text-align: center">Running NoobScript V1.2!</div>').appendTo("#chat-messages")
 
     $('<a id="Madeby" href="http://sd.keepcalm-o-matic.co.uk/i/we-are-sexy-people.png" target="_blank">Running NoobScript</a></div>').appendTo("#app")
-        //$('#Madyby').css('position', 'absolute')
-        //$('#Madyby').css('top', 550);
-        //('#Madyby').css('left', 0)
+    //$('#Madyby').css('position', 'absolute')
+    //$('#Madyby').css('top', 550);
+    //('#Madyby').css('left', 0)
     CT = ["BOW TO US"];
     CMDS = ["cmds", "credits", "NSkill"];
     //Chat commands ^
@@ -27,13 +27,14 @@ function noobScript() {
         //cmds
         if (data === "/" + CMDS[0]) {
             API.chatLog("Commands: " + CMDS.toString(), 1)
-                //credits
+            //credits
         } else if (data === "/" + CMDS[1]) {
             $('<div class="BasicText" style ="color: #4169E1; border: 1px solid #D3D3D3; font-size: 12pt; font-family: Lucida Console; text-align: center">Made By donvoo & SnakedMusique</div>').appendTo("#chat-messages")
-                //kill
+            //kill
         } else if (data === "/" + CMDS[2]) {
             API.chatLog("OH GOD WHY WOULD YOU DO THAT!?!?!");
-            throw "Killing NoobScript";
+          API.off()
+            noobScript = null
         }
     }
 
@@ -47,5 +48,14 @@ function noobScript() {
         }
     }
 }
+
+/*
+API.on(API.CHAT_COMMAND, function NSkill(data){
+    if (data === "/" + CMDS[2]) {
+            API.chatLog("OH GOD WHY WOULD YOU DO THAT!?!?!");
+            throw new Error("Killing NoobScript-331");
+        }
+    });
+*/
 //https://rawgit.com/vav17/NoobScript-331/master/NoobScript.js
 //javascript:(function(){$.getScript('https://rawgit.com/vav17/NoobScript-331/master/NoobScript.js');}());
