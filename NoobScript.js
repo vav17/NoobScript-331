3fc8a17a9829810630abe8dca47288c007169d0c
vav17 <mail@vav17.com>
2015-02-01 01:42:10 +0100

    adding back shit cuz git remved it....

--- NoobScript.js
+++ NoobScript.js
@@ -1,7 +1,4 @@
 //Script Created by Snaked and donvoo
-/*Message For last update:
-donvoo - I made the /cmd to /cmds so it dosn't interfear with origem's /cmd also updated the chat commands and now its working V1.2 <-update the version when you make a successful update
-*/
 function startScript() {
     if (typeof API !== "object" || !API.enabled) {
         setTimeout(startScript, 200);
@@ -10,40 +7,46 @@ function startScript() {
     }
 }
 setTimeout(startScript, 200);
+
 function noobScript() {
     //onStartup
     $('<link type="text/css" rel="Styles" href="https://rawgit.com/vav17/NoobScript-331/master/Styles.css"/>').appendTo("#app")
     $('<div id="Intro" style ="color: #4169E1; border: 1px solid #D3D3D3; font-size: 14pt; font-family: Lucida Console; text-align: center">Running NoobScript V1.2!</div>').appendTo("#chat-messages")
-    
+
     $('<a id="Madeby" href="http://sd.keepcalm-o-matic.co.uk/i/we-are-sexy-people.png" target="_blank">Running NoobScript</a></div>').appendTo("#app")
-    //$('#Madyby').css('position', 'absolute')
-    //$('#Madyby').css('top', 550);
-    //('#Madyby').css('left', 0)
+        //$('#Madyby').css('position', 'absolute')
+        //$('#Madyby').css('top', 550);
+        //('#Madyby').css('left', 0)
     CT = ["BOW TO US"];
     CT = ["WhoOurScript"];
     CMDS = ["cmds", "credits", "kill"];
     //Chat commands ^
     API.on(API.CHAT_COMMAND, commands);
+
     function commands(data) {
         this.data = data;
-        	//cmds
+        //cmds
         if (data === "/" + CMDS[0]) {
-            API.chatLog("Commands: " + CMDS.toString(),1)
-            //credits
-        }else if(data === "/" + CMDS[1]) {
+            API.chatLog("Commands: " + CMDS.toString(), 1)
+                //credits
+        } else if (data === "/" + CMDS[1]) {
             $('<div class="BasicText" style ="color: #4169E1; border: 1px solid #D3D3D3; font-size: 12pt; font-family: Lucida Console; text-align: center">Made By donvoo & SnakedMusique</div>').appendTo("#chat-messages")
-            //kill
-        }else if(data === "/" + CMDS[2]) {
-        	API.chatLog("OH GOD WHY WOULD YOU DO THAT!?!?!");
-        	throw "Killing NoobScript";
+                //kill
+        } else if (data === "/" + CMDS[2]) {
+            API.chatLog("OH GOD WHY WOULD YOU DO THAT!?!?!");
+            throw "Killing NoobScript";
         }
     }
 
     //Chat Triggers
     API.on(API.CHAT, chatKey);
-    function chatKey(keyData){
-        if (/BOW TO US/i.test(keyData.message) && API.getUser().id === 4251764 || API.getUser().id ===  4537120) {
+
+    function chatKey(keyData) {
+        if (/BOW TO US/i.test(keyData.message) && API.getUser().id === 4251764 || API.getUser().id === 4537120) {
             API.sendChat("*Bows*");
+        }
+    }
+}
 
 //https://rawgit.com/vav17/NoobScript-331/master/NoobScript.js
 //javascript:(function(){$.getScript('https://rawgit.com/vav17/NoobScript-331/master/NoobScript.js');}());
