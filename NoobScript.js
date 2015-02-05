feed93c9278e1d7ded982c8085cecb4c378c1eec
donvoo <donvoowright@aol.com>
2015-02-04 18:22:45 -0700

    updated the array emotes

--- NoobScript.js
+++ NoobScript.js
@@ -43,7 +43,7 @@ var noobScript = function() {
     Devs = ["donvoo", "SnakedMusique"];
     CT = ["BOW TO US"];
     CMDS = ["cmds", "credits", "NSkill", "raw", "NSreload", "banned", "api", "noskip", "YES", "dafaq", "EHD", "sowwy", "copysong", "hahano", "nameSaid", "emotelist"];
-    EmoteList = ["twerk", "SNM", "SnakedMusique", "CarltonDance", "Fireworks", "Headbang", "HighShere", "rspin", "jenny", "totoro", "amaze", "amazegif", "kawaii", "spamgif", "banned", "stitchbra", "stitchglasses", "rainbowllama", "dorito", "DNN", "dotaaxerage", "dotachicken", "dotahorse", "dotaaxecry", "nat", "crikawaii"];
+    EmoteList = ["twerk", "SNM", "SnakedMusique", "CarltonDance", "Fireworks", "Headbang", "HighShere", "rspin", "jenny", "totoro", "amaze", "amazegif", "kawaii", "spamgif", "banned", "stitchbra", "stitchglasses", "rainbowllama", "dorito", "DNN", "dotaaxerage", "dotachicken", "dotahorse", "dotaaxecry", "nat", "crikawaii", "pug", "noice"];
     autoWootSet = 0;
 
     API.on(API.CHAT_COMMAND, commands);
