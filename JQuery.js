28296d5d15b17a60447b505d91c56f05b55f2899
vav17 <vincent.voet@hotmail.com>
2015-02-10 00:45:38 +0100

    Update JQuery.js

--- JQuery.js
+++ JQuery.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 	console.log("JQuery: Imported!");
-	$('<script type="text/javascript" src="https://code.jquery.com/ui/jquery-ui-git.js"></script>');
+	$('<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>');
 	$("#Stat").draggable();
 });
