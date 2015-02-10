59e82fd7eea739c48a1ab5681f3c028ece252197
vav17 <mail@vav17.com>
2015-02-10 01:37:59 +0100

    please work :c

--- JQuery.js
+++ JQuery.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 	console.log("JQuery: Imported!");
-	$('<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>').appendTo('head');
+	$.getScript("https://code.jquery.com/ui/jquery-ui-git.js",function(){console.log("SUCCES!")})
 	$("#Stat").draggable();
 });
