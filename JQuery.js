43a7eef9581f27fb71d982583adf9e2c9625c095
vav17 <mail@vav17.com>
2015-02-10 01:24:35 +0100

    again, Im a fucking noob :D

--- JQuery.js
+++ JQuery.js
@@ -2,5 +2,4 @@ $(document).ready(function(){
 	console.log("JQuery: Imported!");
 	$('<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>').appendTo('head');
 	$("#Stat").draggable();
-	})
 });
