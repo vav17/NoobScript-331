ced9d4ec88404e28d8583a0af1e7ee52b1ae9f58
vav17 <mail@vav17.com>
2015-02-10 19:18:09 +0100

    Adding Old Origem chat colors ^^

--- JQuery.js
+++ JQuery.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
 	console.log("JQuery: Imported!");
 	$.getScript("https://code.jquery.com/ui/jquery-ui-git.js",function(){
 		console.log("SUCCES!")
-		$("#Stat").draggable();
+		$('head').append('<link id="cssstaffcolors" rel="stylesheet"type="text/css"href="https://dl.dropboxusercontent.com/s/usedz76mi8km5rq/Origem%20Colors.css">');
+		//$("#Stat").draggable();
 
 		$("#copysong").fadeTo('fast',0);
 		$("#copysong").hover(function(){
