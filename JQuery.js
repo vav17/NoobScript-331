98f55116222e9c2f5f838ab12a04a6489253675a
vav17 <mail@vav17.com>
2015-02-10 02:48:51 +0100

    Trying something.. :P

--- JQuery.js
+++ JQuery.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
 	console.log("JQuery: Imported!");
-	$.getScript("https://code.jquery.com/ui/jquery-ui-git.js",function(){console.log("SUCCES!")})
-	$("#Stat").draggable();
-});
-
-$("#copysong").fadeTo('slow',0)
-$(document).ready(function(){
-	$("#copysong").mouseenter(function(){
-		$("#copysong").fadeTo('slow',1)
-	})
-	$("#copysong").mouseleave(function(){
+	$.getScript("https://code.jquery.com/ui/jquery-ui-git.js",function(){
+		console.log("SUCCES!")
+		$("#Stat").draggable();
+	
+	
 		$("#copysong").fadeTo('slow',0)
-	})
-})
+		$("#copysong").mouseenter(function(){
+			$("#copysong").fadeTo('slow',1)
+		})
+		$("#copysong").mouseleave(function(){
+			$("#copysong").fadeTo('slow',0)
+		});
+	});
+});
