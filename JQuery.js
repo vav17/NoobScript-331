575db3b9ef168050da388a9d71acd50611230292
vav17 <mail@vav17.com>
2015-02-10 03:08:02 +0100

    shit

--- JQuery.js
+++ JQuery.js
@@ -3,14 +3,18 @@ $(document).ready(function(){
 	$.getScript("https://code.jquery.com/ui/jquery-ui-git.js",function(){
 		console.log("SUCCES!")
 		$("#Stat").draggable();
-	
+
 	
 		$("#copysong").fadeTo('slow',0)
 		$("#copysong").mouseenter(function(){
 			$("#copysong").fadeTo('slow',1)
-		})
+		});
 		$("#copysong").mouseleave(function(){
 			$("#copysong").fadeTo('slow',0)
 		});
+
+		//Interface
+		$('<div id="InterfaceToggle"></div>').appendTo("#facebook-menu")
+
 	});
 });
