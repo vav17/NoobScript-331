5b9edb502df8cee4ed0cd6aefd0fcdf3c0e3da9b
donvoo <donvoowright@aol.com>
2015-02-15 16:50:11 -0700

    removed  cause probems

--- NoobScript.js
+++ NoobScript.js
@@ -37,8 +37,6 @@ var noobScript = function() {
     autoWootSet = 0;
     skipTestVar = 1;
     NSloadSettings();
-    $.getScript("https://dl.dropboxusercontent.com/s/f6n5bbclbqbkzfl/noobscripttest.js");
-    
     
     API.on(API.CHAT_COMMAND, commands);
     function commands(data) {
