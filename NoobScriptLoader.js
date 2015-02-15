de8f26488b0df73be7e817fb721f8dd1cdf8ddb3
donvoo <donvoowright@aol.com>
2015-02-15 16:03:26 -0700

    Update NoobScriptLoader.js

--- NoobScriptLoader.js
+++ NoobScriptLoader.js
@@ -1,11 +1,11 @@
 function startScript() {
     if (typeof API !== "object" || !API.enabled) {
-        setTimeout(startScript, 200);
+        $(document).ready(startScript();)
     } else {
         loader();
     }
 }
-$(document).ready(function(){startScript();})
+$(document).ready(startScript();)
 
 function loader(){
 	$.getScript('https://rawgit.com/vav17/NoobScript-331/master/NoobScript.js');
