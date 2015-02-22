6cfd96644c7f49faf89576f71054ec1bb7488595
donvoo <donvoowright@aol.com>
2015-02-21 21:06:37 -0700

    tests

--- JQuery.js
+++ JQuery.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 	console.log("JQuery: Imported!");
-	$.getScript("https://dl.dropboxusercontent.com/s/9vtr4n2ytphy0ra/jquery-ui-1.10.4.custom.js",function(){
 		console.log("SUCCES!")
 		$('head').append('<link id="cssstaffcolors" rel="stylesheet"type="text/css"href="https://dl.dropboxusercontent.com/s/usedz76mi8km5rq/Origem%20Colors.css">');
 		$("#Stat").draggable();
@@ -13,6 +12,4 @@ $(document).ready(function(){
 		});
 
 		//Interfac!@#$wwwwe
-
-	});
 });
