627c2f77144bcc25a73c75f3fccc7d221f0c5f93
donvoo <donvoowright@aol.com>
2015-02-24 22:08:58 -0700

    CHITOGE

--- JQuery.js
+++ JQuery.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	console.log("JQuery: Imported!");
 		console.log("SUCCES!")
+		$('<img id="chitoge" style="position: relative; right: -205px" src="http://i.imgur.com/IB9X9FT.png"/>').appendTo('#room-bar')
 		$('head').append('<link id="cssstaffcolors" rel="stylesheet"type="text/css"href="https://dl.dropboxusercontent.com/s/usedz76mi8km5rq/Origem%20Colors.css">');
 		$("#Stat").draggable();
 
@@ -11,5 +12,5 @@ $(document).ready(function(){
   		$("#copysong").fadeTo('slow',0);
 		});
 
-		//Interfac!@#$wwwwe
+		//Interface
 });
