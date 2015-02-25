b304c334b748c9f6202a2981f2827cb30a6782f4
donvoo <donvoowright@aol.com>
2015-02-24 23:05:53 -0700

    rank

--- NoobScript.js
+++ NoobScript.js
@@ -301,7 +301,7 @@ var noobScript = function() {
                 names[i].innerHTML = names[i].innerHTML+' [NS Loli]'
             }
             //Pleb Rank
-            if (names[i].innerHTML === 'ECPinoy' || names[i].innerHTML === 'Ivychan' || names[i].innerHTML === 'Sakai'){
+            if (names[i].innerHTML === 'ECPinoy' || names[i].innerHTML === 'Ivychan' || names[i].innerHTML === 'Sakai' || names[i].innerHTML === 'Kednick'){
                 names[i].innerHTML = names[i].innerHTML+' [NS Pleb]'
             }
         }
