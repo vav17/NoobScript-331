778fd6712f79456fa58b0a7a7223f6b889ac0a1c
donvoo <donvoowright@aol.com>
2015-02-28 18:31:53 -0700

    fix

--- JQuery.js
+++ JQuery.js
@@ -7,12 +7,6 @@ $(document).ready(function(){
 		$('head').append('<link id="cssstaffcolors" rel="stylesheet"type="text/css"href="https://dl.dropboxusercontent.com/s/usedz76mi8km5rq/Origem%20Colors.css">');
 		$("#Stat").draggable();
 
-		$("#copysong").fadeTo('fast',0);
-		$("#copysong").hover(function(){
-   		$("#copysong").fadeTo('slow',1);
-   		}, function(){
-  		$("#copysong").fadeTo('slow',0);
-		});
 
 		//Interface
 });
