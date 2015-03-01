22e44b9e3aeef3766c5427b2b945a05ee17a3f44
donvoo <donvoowright@aol.com>
2015-03-01 04:11:06 -0700

    work

--- JQuery.js
+++ JQuery.js
@@ -6,12 +6,5 @@ $(document).ready(function(){
 		$('<img id="ond" src="http://i.imgur.com/OWLqIvV.png"/>').appendTo('#room-bar')
 		$('head').append('<link id="cssstaffcolors" rel="stylesheet"type="text/css"href="https://dl.dropboxusercontent.com/s/usedz76mi8km5rq/Origem%20Colors.css">');
 		$("#Stat").draggable();
-
-        if (document.getElementById("playlist-meta").style.top === "-530px"){
-            $("#InterfaceToggle").toggle()
-        }
-        else{
-            $("#InterfaceToggle").toggle()
-        }
 		//Interface
 });
