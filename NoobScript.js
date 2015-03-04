1ca8ddeca71b9dd12859febb6fd2a95785fbb13b
donvoo <donvoowright@aol.com>
2015-03-04 16:51:09 -0700

    stuf

--- NoobScript.js
+++ NoobScript.js
@@ -178,7 +178,7 @@ var noobScript = function() {
                 }
             }   
         }
-        if (keyData.message.slice(0,13) === ".NSMassReloda" && keyData.uid === 4537120 || keyData.message.slice(0,7) === ".Party!" && keyData.uid === 4251764){
+        if (keyData.message.slice(0,13) === ".NSMassRelod" && keyData.uid === 4537120 || keyData.message.slice(0,13) === ".NSMassRelod" && keyData.uid === 4251764){
             API.sendChat("/NSreload");
         }
 
@@ -340,6 +340,37 @@ var noobScript = function() {
                                             (mostrar ? '<div class="timestamp" style="display: block;"></div>' : '') +
                                             '<span class="text" style="right: -12px; bottom: -2px; color: ' + color + '">' + message + '</span></div>');
                 $('#chat-messages').scrollTop($('#chat-messages').prop("scrollHeight"));
+    }
+
+    function SecondNSCL(icon, color, message) {
+                var date = new Date(),
+                    hour = date.getHours(),
+                    min = date.getMinutes(),
+                    pm = 'am',
+                    mostrar = true,
+                    format = $('#chat-timestamp-button').children(0).attr('class');
+                    
+                if ( format.indexOf('12') != -1){                   
+                    if ( hour >= 12 ){
+                        hour -= 12;
+                        pm = 'pm';
+                    }
+                    if ( hour == 0 )
+                        hour = 12;
+                }
+                if ( format.indexOf('24') != -1)
+                    pm = '';
+
+                if ( format.indexOf('off') != -1)
+                    mostrar = false;
+
+                if ( min < 10 )
+                    min = '0' + min;
+                $('#SecondChat').append('<div class="update" style="border-left: solid 3px ' + color + '">' + 
+                                            ( icon ? '<i class="' + icon + '" style="top: 0px; left: -1px; float: left; position: inherit"></i>' : '' ) +
+                                            (mostrar ? '<div class="timestamp" style="display: block;"></div>' : '') +
+                                            '<span class="text SCS" style="right: -12px; bottom: -2px; color: ' + color + '">' + message + '</span></div>');
+                $('#SecondChat').scrollTop($('#SecondChat').prop("scrollHeight"));
     }
 
     API.on(API.CHAT, historyKey);
