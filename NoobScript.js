f534c1b26208f9429359023f16d43d22f55e4927
donvoo <donvoowright@aol.com>
2015-03-05 16:32:48 -0700

    FIX NOW PLS

--- NoobScript.js
+++ NoobScript.js
@@ -366,7 +366,7 @@ var noobScript = function() {
 
                 if ( min < 10 )
                     min = '0' + min;
-                $('#SecondChat').append('<div class="update" style="border: 1px solid white'">' + 
+                $('#SecondChat').append('<div class="update" style="border: 1px solid white">' + 
                                             ( icon ? '<i class="' + icon + '" style="top: 0px; left: -1px; float: left; position: fixed"></i>' : '' ) +
                                             (mostrar ? '<div class="timestamp" style="display: block;"></div>' : '') +
                                             '<span class="text" style="right: -12px; bottom: -2px; color: ' + color + '">' + message + '</span><br></div>');
