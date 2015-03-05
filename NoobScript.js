56cc6466b57ff16bc75bb97ef2e89e12b0116396
donvoo <donvoowright@aol.com>
2015-03-05 16:59:51 -0700

    more fix

--- NoobScript.js
+++ NoobScript.js
@@ -335,7 +335,7 @@ var noobScript = function() {
 
                 if ( min < 10 )
                     min = '0' + min;
-                $('#chat-messages').append('<div class="update" style="height: 26px; border-left: solid 3px ' + color + '">' + 
+                $('#chat-messages').append('<div class="update" style="height: 30px; border-left: solid 3px ' + color + '">' + 
                                             ( icon ? '<i class="' + icon + '" style="top: 0px; left: -1px; float: left; position: relative"></i>' : '' ) +
                                             (mostrar ? '<div class="timestamp" style="display: block;"></div>' : '') +
                                             '<span class="text" style="position: relative; right: -12px; bottom: -2px; color: ' + color + '">' + message + '</span><br></div>');
@@ -366,7 +366,7 @@ var noobScript = function() {
 
                 if ( min < 10 )
                     min = '0' + min;
-                $('#SecondChat').append('<div class="update" style="height: 26px; border: solid 1px ' + color + '">' + 
+                $('#SecondChat').append('<div class="update" style="height: 30px; border: solid 1px ' + color + '">' + 
                                             ( icon ? '<i class="' + icon + '" style="top: 0px; left: -1px; float: left; position: relative"></i>' : '' ) +
                                             (mostrar ? '<div class="timestamp" style="display: block;"></div>' : '') +
                                             '<span class="text" style="position: relative; right: -12px; bottom: -2px; color: ' + color + '">' + message + '</span><br></div>');
