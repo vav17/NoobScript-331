6d4c0a94af708b5887d5ff5f9d5d115449d65030
donvoo <donvoowright@aol.com>
2015-03-05 17:12:06 -0700

    more fix

--- NoobScript.js
+++ NoobScript.js
@@ -369,7 +369,7 @@ var noobScript = function() {
                 $('#SecondChat').append('<div class="update" style="height: 50px; border: solid 1px ' + color + '">' + 
                                             ( icon ? '<i class="' + icon + '" style="top: 0px; left: -1px; float: left; position: relative"></i>' : '' ) +
                                             (mostrar ? '<div class="timestamp" style="display: block;"></div>' : '') +
-                                            '<span class="text" style="position: relative; right: -12px; bottom: -2px; color: ' + color + '">' + message + '</span><br></div>');
+                                            '<span class="text" style="position: relative; bottom: -2px; color: ' + color + '">' + message + '</span><br></div>');
                 $('#SecondChat').scrollTop($('#SecondChat').prop("scrollHeight"));
     }
 
