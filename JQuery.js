ab8d71356b3df635e301dcd22122d339f9f72804
donvoo <donvoowright@aol.com>
2015-03-07 15:28:46 -0700

    nice change

--- JQuery.js
+++ JQuery.js
@@ -6,5 +6,5 @@ $(document).ready(function(){
 		$('<img id="ond" src="http://i.imgur.com/OWLqIvV.png"/>').appendTo('#room-bar');
 		$('head').append('<link id="cssstaffcolors" rel="stylesheet"type="text/css"href="https://dl.dropboxusercontent.com/s/usedz76mi8km5rq/Origem%20Colors.css">');
 		$("#Stat").draggable();
-		$('#chat-messages').append('<div class="NSalert">heyo! thanks for using our FUCKING NOOBSCRIPT</div>');
+		$('#chat-messages').append('<div class="NSalert">Heyo! Thanks For Using This Script <3 !</div>');
 });
